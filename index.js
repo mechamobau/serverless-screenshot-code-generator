28d8780e7e3ac8dc4cbee5f9a70ae345535663d4
Lucas Viana <vianadossantoslucas@gmail.com>
2020-07-18 17:07:36 -0300

    :construction: adds extractor to sent code in request

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+const REGEX = /#CodeBot\s(\w+)\n(((.*)\n)+)/;
+
+const isValidContent = (content) => {
+	return REGEX.test(content);
+};
+
 exports.helloWorld = (req, res) => {
-	res.send("Hello World!");
+	try {
+		if (!req.body.content)
+			throw new Error(
+				"You need to provide the content of tweet inside the body of request"
+			);
+
+		const { content } = req.body;
+
+		if (isValidContent(content)) {
+			const contentMatches = content.match(REGEX);
+
+			const language = contentMatches[1];
+
+			const code = contentMatches[2];
+
+			res.send({
+				language,
+				code,
+			});
+		} else {
+			throw new Error("Not a valid code");
+		}
+	} catch (e) {
+		res.status(500).send({
+			data: {
+				error: e.message,
+			},
+		});
+	}
 };
